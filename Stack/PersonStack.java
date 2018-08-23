a96fc2e54c6d7fe1dae518cbe6ea66c38cc55f77
jk2618 <38773092+jk2618@users.noreply.github.com>
2018-08-22 22:19:34 -0500

    Update PersonStack.java

--- Stack/PersonStack.java
+++ Stack/PersonStack.java
@@ -1,1 +1,39 @@
+public class PersonStack {
+    private Person [] stack; // integer array
+    private int top; // the last index
+    private int size;
 
+    public PersonStack(){ // constructor
+        top = -1;
+        size = 50;
+        stack = new Person [50];
+    }
+
+    public PersonStack(int size){
+        top = -1;
+        this.size = size;
+        stack = new Person [this.size];
+    }
+
+    public boolean push(Person item){
+        if (!isFull()){
+            top++;
+            stack[top] = item;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean isFull(){
+        return (top == stack.length - 1);
+    }
+
+    public Person pop(){
+        return stack[top--];
+    }
+
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+}
