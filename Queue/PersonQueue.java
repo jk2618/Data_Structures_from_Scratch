e69ea0491cc9ddf28ec041dda0af3ad024b03f5d
jk2618 <38773092+jk2618@users.noreply.github.com>
2018-08-22 22:28:11 -0500

    Create PersonQueue.java

---
+++ Queue/PersonQueue.java
@@ -0,0 +1,59 @@
+public class PersonQueue {
+    private Person [] q;
+    private int size;
+    private int total;
+    private int front; // index for last element
+    private int rear; // index for first element
+
+    public PersonQueue(){ // constructor
+        size = 100;
+        total = 0;
+        front = 0;
+        rear = 0;
+        q = new Person[size];
+    }
+
+    public PersonQueue(int size){
+        this.size = size;
+        total = 0;
+        front = 0;
+        rear = 0;
+        q = new Person[this.size];
+    }
+
+    public boolean enqueue(Person item){
+        if(isFull()){
+            return false;
+        }else{
+            total++;
+            q[rear] = item;
+            rear = (rear + 1) % size; // purpose ?
+            return true;
+        }
+    }
+
+    public Person dequeue(){
+        Person item = q[front];
+        total--;
+        front = (front + 1) % size; // purpose ?
+        return item;
+    }
+
+    public boolean isFull(){
+        if (size == total){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void showAll(){
+        int f = front;
+        if (total != 0) {
+            for (int i = 0; i < total; i++){
+                System.out.println(" " + q[f].toString());
+                f = (f + 1) % size;
+            }
+        }
+    }
+}
